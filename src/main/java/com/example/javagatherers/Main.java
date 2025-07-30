10dd920f720ae3929c5aeee24168fb856f3cfe70
Alex <alextchatalov@gmail.com>
2025-07-29 23:05:57 -0300

    initial project structure

---
+++ src/main/java/com/example/javagatherers/Main.java
@@ -0,0 +1,15 @@
+package com.example.javagatherers;
+
+/**
+ * Main class for the Java Gatherers application.
+ */
+public class Main {
+    /**
+     * The entry point of the application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Gatherers!");
+    }
+}
